7aca76586b9b3cac76bafc17b4f6734292cc7e80
firebadnofire <71282731+firebadnofire@users.noreply.github.com>
2023-03-25 20:28:50 -0400

    Update zsh1.sh

--- zsh1.sh
+++ zsh1.sh
@@ -12,11 +12,12 @@ echo "8) Exit"
 read -p "Enter your choice (1-7): " choice
 
 if [ $choice -eq 1 ]; then
-    curl https://archuser.org/apt-fast.sh | bash; curl -o ~/aptf https://archuser.org/apt-fast.conf; sudo mv ~/aptf /etc/apt-fast.conf; sudo apt-fast install -y zsh curl git fzy
+    curl https://raw.githubusercontent.com/firebadnofire/zshrc/main/apt-fash.sh | bash; curl -o ~/aptf https://raw.githubusercontent.com/firebadnofire/zshrc/main/apt-fast.conf; sudo mv ~/aptf /etc/apt-fast.conf; sudo apt-fast install -y zsh curl git fzy
 elif [ $choice -eq 2 ]; then
     curl https://download.argon40.com/argon1.sh | bash
 elif [ $choice -eq 3 ]; then
-sudo dnf install zsh git; curl -o fzy-1.0-1.17.x86_64.rpm https://ftp.lysator.liu.se/pub/opensuse/tumbleweed/repo/oss/x86_64/fzy-1.0-1.17.x86_64.rpm; curl -o catimg-2.7.0-4.el9.x86_64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/c/catimg-2.7.0-4.el9.x86_64.rpm; curl -o neofetch-7.1.0-7.el9.noarch.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/n/neofetch-7.1.0-7.el9.noarch.rpm; curl -o screen-4.8.0-6.el9.x86_64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/s/screen-4.8.0-6.el9.x86_64.rpm; sudo dnf install ./*.rpm
+sudo dnf install zsh git; curl -o fzy-1.0-1.17.x86_64.rpm https://ftp.lysator.liu.se/pub/opensuse/tumbleweed/repo/oss/x86_64/fzy-1.0-1.17.x86_64.rpm; curl -o catimg-2.7.0-4.el9.x86_64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/c/catimg-2.7.0-4.el9.x86_64.rpm; curl -o neofetch-7.1.0-7.el9.no
+.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/n/neofetch-7.1.0-7.el9.noarch.rpm; curl -o screen-4.8.0-6.el9.x86_64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/s/screen-4.8.0-6.el9.x86_64.rpm; sudo dnf install ./*.rpm
 elif [ $choice -eq 4 ]; then
 sudo dnf install zsh git; curl -o fzy-1.0-1.15.aarch64.rpm https://ftp.lysator.liu.se/pub/opensuse/ports/aarch64/tumbleweed/repo/oss/aarch64/fzy-1.0-1.15.aarch64.rpm; curl -o neofetch-7.1.0-7.el9.noarch.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/n/neofetch-7.1.0-7.el9.noarch.rpm; curl -o catimg-2.7.0-4.el9.aarch64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/c/catimg-2.7.0-4.el9.aarch64.rpm; curl -o screen-4.8.0-6.el9.aarch64.rpm https://download-ib01.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/screen-4.8.0-6.el9.aarch64.rpm; sudo dnf install ./*.rpm
 elif [ $choice -eq 5 ]; then
@@ -25,7 +26,7 @@ elif [ $choice -eq 6 ]; then
     # Do nothing, continue
     echo Installing configurations...
 elif [ $choice -eq 7 ]; then
-    su - root -c "pkg install zsh git fzy"
+    su - root -c "pkg install zsh fzy"
 elif [ $choice -eq 8 ]; then
     exit
 else
@@ -34,5 +35,5 @@ fi
 mkdir ~/.zshell
 curl -Lo ~/.zshell/zsh-autosuggestions.zsh https://infotoast.org/aka/zshsuggest
 curl -Lo ~/.zshell/zsh-syntax-highlighting.zsh https://infotoast.org/aka/zshsyntax
-curl -o ~/.zshrc https://archuser.org/zshrc
+curl -o ~/.zshrc https://raw.githubusercontent.com/firebadnofire/zshrc/main/zshrc
 echo ""
